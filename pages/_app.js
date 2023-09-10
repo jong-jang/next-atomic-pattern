c9ffccd376d6277385e9fc79c1e59e950023620b
jong-jang <jjh0299@gmail.com>
2023-09-10 10:48:32 +0900

    기본 데이터패칭 url

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Layout from '@/components/template/Layout/Layout';
 
+// 기본 url 설정
 axios.defaults.baseURL = 'https://www.themealdb.com/api/json/v1/1';
 
 const queryClient = new QueryClient();
