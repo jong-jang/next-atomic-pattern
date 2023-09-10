5ee1304d59d537bfe9fc04af16fd84347e6fafee
jong-jang <jjh0299@gmail.com>
2023-09-10 10:48:49 +0900

    메인페이지에 isr방식으로 데이터 전달후 스와이퍼 처리

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
 import Head from 'next/head';
+import axios from 'axios';
+import SwiperWrap from '@/components/organisms/Swiper/Swiper';
 
 export default function Home({ meals, category }) {
+	console.log('카테고리명', category);
+	console.log('카테고리명 배열데이터', meals);
 	return (
 		<>
 			<Head>
 				<title>Main Page</title>
 			</Head>
 
-			<main></main>
+			<main>
+				<SwiperWrap recipe={meals.slice(0, 6)} category={category} />
+			</main>
 		</>
 	);
+}
+
+export async function getStaticProps() {
+	const list = [];
+	const { data: obj } = await axios.get('/categories.php');
+	const items = obj.categories;
+	items.forEach((el) => list.push(el.strCategory));
+	const newList = list.filter((el) => el !== 'Goat' && el !== 'Vegan' && el !== 'Starter');
+	const randomNum = Math.floor(Math.random() * newList.length);
+	const { data } = await axios.get(`/filter.php?c=${newList[randomNum]}`);
+
+	return {
+		props: { ...data, category: newList[randomNum] },
+		revalidate: 60 * 60,
+	};
 }
